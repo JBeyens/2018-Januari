3daf09e280f3e670b9eafd70e20bef004988b6ac
JBeyens <jefbeyens@gmail.com>
2017-11-27 21:04:48 +0100

    start ID module

---
+++ 8-Afstandsbediening/src/model/idmodule/IdModule.java
@@ -0,0 +1,13 @@
+package model.idmodule;
+
+
+/**
+ * @Author Jef Beyens & Ben Vandevorst
+ * @Datum 27/11/2017
+ * @Project Afstandsbediening
+ * @Doel Manages user/remote authorization
+ */
+public class IdModule {
+	
+
+}
