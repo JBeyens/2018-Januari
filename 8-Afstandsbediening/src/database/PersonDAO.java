adba026cf89f267ec56912e8fe03ca1cbd3fc521
MacCrimmon <ben.vandevorst@hotmail.com>
2017-12-02 14:23:48 +0100

    Added Generic DAO Interface Created Person/Remote DAO

---
+++ 8-Afstandsbediening/src/database/PersonDAO.java
@@ -0,0 +1,39 @@
+package database;
+
+import java.util.List;
+
+import model.dataobjects.Person;
+
+public class PersonDAO implements GenericDAO<Person>{
+
+	@Override
+	public List<Person> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Person findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Person entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Person entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void create(Person entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ 8-Afstandsbediening/src/database/RemoteDAO.java
@@ -0,0 +1,39 @@
+package database;
+
+import java.util.List;
+
+import model.dataobjects.Remote;
+
+public class RemoteDAO implements GenericDAO<Remote>{
+
+	@Override
+	public List<Remote> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Remote findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Remote entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Remote entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void create(Remote entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
