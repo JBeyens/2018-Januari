af7fb0892cbf85a9d708bda4e6902bc39b802210
MacCrimmon <ben.vandevorst@hotmail.com>
2017-12-04 18:41:34 +0100

    added facotry example

--- 8-Afstandsbediening/src/test/TestClass.java
+++ 8-Afstandsbediening/src/test/TestClass.java
@@ -1,10 +1,15 @@
 package test;
 
+import org.fluttercode.datafactory.impl.DataFactory;
 
 public class TestClass {
 
 	public static void main(String[] args){
+		DataFactory f = new DataFactory();
 		
+		String name = f.getFirstName();
+		
+		System.out.println(name);
 		
 		
 
