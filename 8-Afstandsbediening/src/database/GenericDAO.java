bafc663d6ca992b4ceaa32866650663bdc02e234
MacCrimmon <ben.vandevorst@hotmail.com>
2017-12-09 14:13:40 +0100

    DAO complete => GenericDAO / going to make testdata

--- 8-Afstandsbediening/src/database/GenericDAO.java
+++ 8-Afstandsbediening/src/database/GenericDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import org.hibernate.HibernateException;
 
@@ -17,7 +18,7 @@ public class GenericDAO<T> {
 	public GenericDAO(Class<T> genericClass) {
 		this.genericClass = genericClass;
 		
-		factory = EntityMgrFactory.getFactory(genericClass.getSimpleName());
+		factory = Persistence.createEntityManagerFactory(genericClass.getSimpleName());
 	}
 	
 	@SuppressWarnings("unchecked")
