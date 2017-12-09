18d137cfa4435011373a518bafddf23c97db8eb5
MacCrimmon <ben.vandevorst@hotmail.com>
2017-12-09 22:47:00 +0100

    Simplified DummyGenerator

--- 8-Afstandsbediening/src/test/database/GenerateDummyData.java
+++ 8-Afstandsbediening/src/test/database/GenerateDummyData.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
+import javax.transaction.Transactional;
+
 import org.fluttercode.datafactory.impl.DataFactory;
 
 import database.GenericDAO;
@@ -18,52 +20,31 @@ public class GenerateDummyData {
 	private static Address address;
 	private static Person person;
 	
-	private static GenericDAO<Remote> remoteDAO = new GenericDAO<>(Remote.class);
-	private static GenericDAO<Address> addressDAO = new GenericDAO<>(Address.class);
 	private static GenericDAO<Person> personDAO = new GenericDAO<>(Person.class);
 	
 	private static DataFactory factory = new DataFactory();
 	
 	public static void main(String[] args) {
-		try {
-			createRemote();
-			createAddress();
-			createPerson();
+		try {;
+			createDate();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
-	
-	private static void createRemote(){
-		for (int i = 0; i < 35; i++) {
-			String serialNumber = UUID.randomUUID().toString();
-			long frequency = ThreadLocalRandom.current().nextLong(10000, 1000000);
-			remote = new Remote(serialNumber, frequency);
-			
-			remoteDAO.create(remote);
-		}
-	}
-	
-	private static void createAddress(){
-		for (int i = 1; i < 31; i++) {
-			address = new Address("Bondgenotenlaan", 150, i, 3000, "Leuven", "België");
-			addressDAO.create(address);
-		}
-	}
 
-	private static void createPerson(){
+	private static void createDate(){
 		LocalDate date  = LocalDate.now();
 		date.plusYears(2);		
 		Date contractDate = Date.valueOf(date);
 		
-		List<Remote> remoteList = remoteDAO.findAll();
-		List<Address> addressList = addressDAO.findAll();
-		
 		for (int i = 0; i < 30; i++) {
 			person = new Person(factory.getFirstName(), factory.getLastName(), contractDate);
-			person.setAdress(addressList.get(i));
-			person.setRemote(remoteList.get(i));
+			address = new Address("Bondgenotenlaan", 150, i, 3000, "Leuven", "België");
+			remote = new Remote(UUID.randomUUID().toString(), ThreadLocalRandom.current().nextLong(10000, 1000000));
+			
+			person.setAdress(address);
+			person.setRemote(remote);
 			
 			personDAO.create(person);
 		}
