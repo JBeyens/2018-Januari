d6b71f618ae72a2372b16a6d8f2d6fe0977e13ae
MacCrimmon <ben.vandevorst@hotmail.com>
2017-12-09 15:02:01 +0100

    GenerateDummyData => remote ok

--- 8-Afstandsbediening/src/test/TestClass.java
+++ 8-Afstandsbediening/src/test/TestClass.java
@@ -22,20 +22,7 @@ public class TestClass {
 		GenericDAO<Remote> RemoteDAO = new GenericDAO<>(Remote.class);
 		GenericDAO<Address> AddressDAO = new GenericDAO<>(Address.class);
 		
-		Remote remote =RemoteDAO.findOne(4);
-		Address address = AddressDAO.findOne(1);
-		
-		try {
-			person = new Person("Ben", "Vandevorst", date, "test");
-			
-			person.setAdress(address);
-			person.setRemote(remote);
-			
-			PersonDAO.create(person);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		RemoteDAO.create(new Remote("Ben", 0));
 		
 		
 
---
+++ 8-Afstandsbediening/src/test/database/GenerateDummyData.java
@@ -0,0 +1,42 @@
+package test.database;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import database.GenericDAO;
+import modelPersistent.Address;
+import modelPersistent.Person;
+import modelPersistent.Remote;
+
+public class GenerateDummyData {
+	private static Remote remote;
+	private static Address address;
+	private static Person person;
+	
+	private static GenericDAO<Remote> remoteDAO = new GenericDAO<>(Remote.class);;
+	private static GenericDAO<Address> addressDAO = new GenericDAO<>(Address.class);;
+	private static GenericDAO<Person> PersonDAO = new GenericDAO<>(Person.class);;
+	
+	private static Random random;
+	
+	public static void main(String[] args) {
+		try {
+			createRemote();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private static void createRemote(){
+		for (int i = 0; i < 35; i++) {
+			String serialNumber = UUID.randomUUID().toString();
+			long frequency = ThreadLocalRandom.current().nextLong(10000, 1000000);
+			remote = new Remote(serialNumber, frequency);
+			
+			remoteDAO.create(remote);
+		}
+	}
+
+}
