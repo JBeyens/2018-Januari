85e90ee482cb7a0dd4dc61050aa8f97464258517
MacCrimmon <ben.vandevorst@hotmail.com>
2017-12-09 22:48:16 +0100

    Removed imports

--- 8-Afstandsbediening/src/test/database/GenerateDummyData.java
+++ 8-Afstandsbediening/src/test/database/GenerateDummyData.java
@@ -2,11 +2,8 @@ package test.database;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
-
-import javax.transaction.Transactional;
 
 import org.fluttercode.datafactory.impl.DataFactory;
 
