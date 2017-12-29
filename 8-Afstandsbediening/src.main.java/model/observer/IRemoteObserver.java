31c97c0283291d9025a8764de75af51c0839f8d9
JBeyens <jefbeyens@gmail.com>
2017-12-29 09:54:51 +0100

    RemoteObserver can return Remote

--- 8-Afstandsbediening/src.main.java/model/observer/IRemoteObserver.java
+++ 8-Afstandsbediening/src.main.java/model/observer/IRemoteObserver.java
@@ -1,6 +1,9 @@
 package model.observer;
 
+import model.entities.Remote;
+
 public interface IRemoteObserver {
 	public void updateFrequency(long frequency);
 	public String sendSerialId();
+	public Remote getRemote();
 }
