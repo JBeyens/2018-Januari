a8fa1b9d764042a0c0a4f85be7be07277606a2d2
JBeyens <jefbeyens@gmail.com>
2018-01-01 18:42:06 +0100

    fixed reference to logger

--- 8-Afstandsbediening/src.main.java/test/database/GenerateDummyData.java
+++ 8-Afstandsbediening/src.main.java/test/database/GenerateDummyData.java
@@ -2,16 +2,19 @@ package test.database;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.log4j.Logger;
 import org.fluttercode.datafactory.impl.DataFactory;
 
-import database.GenericDAO;
+import antlr.collections.List;
 import model.entities.Address;
+import model.entities.EntityDAO;
 import model.entities.Person;
 import model.entities.Remote;
-import values.DefaultLogger;
+import values.DefaultSettings;
 
 
 /**
@@ -25,56 +28,71 @@ public class GenerateDummyData {
 	private static Remote remote;
 	private static Address address;
 	private static Person person;
-	
+
+	private static Logger logger = DefaultSettings.getLogger();
+	private static Date contractDate = getDate();	
+	private static Integer amountOfAddresses = 30;
 	private static DataFactory factory = new DataFactory();
 	
 	public static void main(String[] args) {
-		DefaultLogger.configureLogger();
-		try {;
-			createActivePerson();
+		try {
+			createActiveRemotesPersonsAddresses();
 			createInActiveRemote();
+			createInActivePerson();
 			createInActiveAddress();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
-
-	private static void createActivePerson(){
-		GenericDAO<Person> personDAO = new GenericDAO<>(Person.class);
-		
+	
+	private static Date getDate() {		
 		LocalDate date  = LocalDate.now();
 		date.plusYears(2);		
-		Date contractDate = Date.valueOf(date);
+		return Date.valueOf(date);		
+	} 
+
+	private static void createActiveRemotesPersonsAddresses(){	
+		logger.info("Creating linked remotes, persons and addresses...");	
 		
-		for (int i = 0; i < 30; i++) {
-			person = new Person(factory.getFirstName(), factory.getLastName(), contractDate);
-			address = new Address("Bondgenotenlaan", 150, i+1, 3000, "Leuven", "Belgi�");
+		
+		for (int i = 1; i <= amountOfAddresses; i++) {
 			remote = new Remote(UUID.randomUUID().toString(), ThreadLocalRandom.current().nextLong(10000, 1000000));
+			person = new Person(factory.getFirstName(), factory.getLastName(), contractDate);
+			address = new Address("Bondgenotenlaan", 150, i+1, 3000, "Leuven", "Belgi" + Character.toString((char)235));
 			
-			person.setAdress(address);
-			remote.setPerson(person);
 			remote.setIsActive(true);
+			remote.setPerson(person);
+			person.setAdress(address);
 			
-			personDAO.create(person);
+			EntityDAO.REMOTE_DAO.create(remote);
 		}
 	}
 	
-	private static void createInActiveRemote(){
-		GenericDAO<Remote> remoteDAO = new GenericDAO<>(Remote.class);
-		for (int i = 0; i < 10; i++) {
+	private static void createInActiveRemote(){		
+		logger.info("Creating remotes not linked to persons...");
+		for (int i = 1; i < 10; i++) {
 			remote = new Remote(UUID.randomUUID().toString(), ThreadLocalRandom.current().nextLong(10000, 1000000));
 			
-			remoteDAO.create(remote);
+			EntityDAO.REMOTE_DAO.create(remote);
+		}
+	}
+	
+	private static void createInActivePerson(){
+		logger.info("Creating persons not linked by remotes...");
+		for (int i = 1; i <= 10; i++) {
+			person = new Person(factory.getFirstName(), factory.getLastName(), contractDate);
+			
+			EntityDAO.PERSON_DAO.create(person);
 		}
 	}
 	
 	private static void createInActiveAddress(){
-		GenericDAO<Address> addressDAO = new GenericDAO<>(Address.class);
-		for (int i = 0; i < 10; i++) {
-			address = new Address("Bondgenotenlaan", 150, i+31, 3000, "Leuven", "Belgi�");
+		logger.info("Creating addresses not linked by persons...");
+		for (int i = 1; i <= 10; i++) {
+			address = new Address("Bondgenotenlaan", 150, amountOfAddresses + i, 3000, "Leuven", "Belgi" + Character.toString((char)235));
 			
-			addressDAO.create(address);
+			EntityDAO.ADDRESS_DAO.create(address);
 		}
 	}
 }
