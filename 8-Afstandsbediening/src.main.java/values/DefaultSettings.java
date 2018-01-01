57cdcad5583d30cdc5d8743212703844b3290164
JBeyens <jefbeyens@gmail.com>
2018-01-01 18:40:53 +0100

    Delete defaultLogger, since DefaultSettings contains defaultLogger now

--- 8-Afstandsbediening/src.main.java/values/DefaultSettings.java
+++ 8-Afstandsbediening/src.main.java/values/DefaultSettings.java
@@ -2,6 +2,7 @@ package values;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class DefaultSettings {
 	private static Logger logger;
@@ -9,7 +10,9 @@ public class DefaultSettings {
 	public static Logger getLogger() {
 		if (logger != null)
 			return logger;
-		
+
+		// Configure log4j settings:
+		DOMConfigurator.configure("src.main.java/log4j.xml");
 		logger = Logger.getLogger("Log");
 		logger.setLevel(Level.ALL);
 		return logger;
