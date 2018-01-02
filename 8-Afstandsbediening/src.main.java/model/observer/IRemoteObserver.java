7958793eb5190e7b21f042945bb9dc3568353269
JBeyens <jefbeyens@gmail.com>
2018-01-02 10:46:00 +0100

    Observer pattern for communication between datamanager & gateModule

--- 8-Afstandsbediening/src.main.java/model/observer/IRemoteObserver.java
+++
@@ -1,9 +0,0 @@
-package model.observer;
-
-import model.entities.Remote;
-
-public interface IRemoteObserver {
-	public void updateFrequency(long frequency);
-	public String sendSerialId();
-	public Remote getRemote();
-}
--- 8-Afstandsbediening/src.main.java/model/observer/IRemoteSubject.java
+++
@@ -1,6 +0,0 @@
-package model.observer;
-
-public interface IRemoteSubject {
-	public void registerUserRemote(IRemoteObserver remote);
-	public void deactivateUserRemote(IRemoteObserver remote);
-}
