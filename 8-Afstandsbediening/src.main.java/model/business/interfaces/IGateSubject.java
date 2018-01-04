0435787149a50a59c943b64232a863f0a0ef8963
JBeyens <jefbeyens@gmail.com>
2018-01-04 19:17:13 +0100

    Interface IGateSubject not used anymore

--- 8-Afstandsbediening/src.main.java/model/business/interfaces/IGateSubject.java
+++
@@ -1,9 +0,0 @@
-package model.business.interfaces;
-
-public interface IGateSubject {
-	public void registerGate(IGateObserver gateModule);
-	public void unregisterGate(IGateObserver gateModule);
-	
-	// add / remove / notify functie => void return type allemaal
-	// concrete subject heeft dan lijst van igateobserver uiteraard
-}
