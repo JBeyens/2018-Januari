898885aeb8ff280ae6e49a213199fb6b985ce29f
MacCrimmon <ben.vandevorst@hotmail.com>
2018-01-06 15:01:17 +0100

    user check update

--- 8-Afstandsbediening/src.main.java/model/business/User.java
+++ 8-Afstandsbediening/src.main.java/model/business/User.java
@@ -25,6 +25,9 @@ public class User implements AdminObserver{
 
 	@Override
 	public void update(long frequency) {
-		getPerson().getRemote().setFrequency(frequency);
+		//If remote not active, it cannot be updated
+		if(getPerson().getRemote().getIsActive())
+			getPerson().getRemote().setFrequency(frequency);
 	}
+	
 }
