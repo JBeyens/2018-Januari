665ad26e5de908a96332d27146467097a632576b
JBeyens <jefbeyens@gmail.com>
2018-01-07 02:00:09 +0100

    Added strings

--- 8-Afstandsbediening/src.main.java/values/DeactivatePersonResult.java
+++ 8-Afstandsbediening/src.main.java/values/DeactivatePersonResult.java
@@ -1,7 +1,17 @@
 package values;
 
 public enum DeactivatePersonResult {
-	Succesfull,
-	NoRemote,
-	NotFound
+	succesfull("Person was succesfully deactivated!"),
+	notFound("Person could not be deactivated because person was not found as registered user!");
+	
+	private final String deactivatePersonResult;
+	
+	DeactivatePersonResult(String deactivatePersonResult){
+		this.deactivatePersonResult = deactivatePersonResult;
+	}
+	
+	@Override
+	public String toString(){
+		return deactivatePersonResult;
+	}
 }
