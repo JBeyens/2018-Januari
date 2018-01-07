e28a323e7f149b7bd8ca264ff2d9bbd8dcbb51fa
JBeyens <jefbeyens@gmail.com>
2018-01-07 02:01:24 +0100

    List of Users instead of AdminObservers. Removed MAGIC STRING as return! Very dangerous & bad practice!!!

--- 8-Afstandsbediening/src.main.java/model/business/Administrator.java
+++ 8-Afstandsbediening/src.main.java/model/business/Administrator.java
@@ -6,41 +6,40 @@ import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
-import model.business.interfaces.AdminObserver;
 import model.business.interfaces.AdminSubject;
 import model.business.DataManager;
 import model.entities.Person;
 import model.entities.Remote;
+import net.bytebuddy.asm.Advice.Return;
 import values.RegisterPersonResult;
 import values.DefaultSettings;
 import values.DeactivatePersonResult;
 
 public class Administrator implements AdminSubject{
+	/** FIELDS **/
 	private Logger log;
 	private long frequency;
-	private ArrayList<AdminObserver> listeners;
+	private ArrayList<User> users;
 	
+	/** CONSTRUCTOR **/
 	public Administrator(){
 		log = DefaultSettings.getLogger(this.getClass().getSimpleName());
 		this.setFrequency(DefaultSettings.RANDOM.nextLong());
-		this.listeners = new ArrayList<>();
+		this.users = new ArrayList<>();
 		loadListenersFromDB();
 	}
 
+	/** METHODS **/
 	/**
-	 * Getter & setter for list of subscribed listeners
+	 * Getter for list of subscribed users
 	 **/
-	public ArrayList<AdminObserver> getListeners() {
-		log.debug("Getting list of Listeners");
-		return listeners;
-	}
-	public void setListeners(ArrayList<AdminObserver> listeners) {
-		log.debug("Setting list of Listeners");
-		this.listeners = listeners;
+	public ArrayList<User> getListeners() {
+		log.debug("Getting list of users");
+		return users;
 	}
 	
 	/**
-	 * Getter & setter for frequency
+	 * Getter & setter for frequency. Setter will also notify all saved 
 	 **/
 	public long getFrequency() {
 		log.debug("Getting frequency");
@@ -49,6 +48,7 @@ public class Administrator implements AdminSubject{
 	public void setFrequency(long frequency) {
 		log.debug("Setting frequency");
 		this.frequency = frequency;
+		notifyAllObservers();
 	}
 
 	
@@ -59,40 +59,45 @@ public class Administrator implements AdminSubject{
 		ArrayList<Person> allPersons = DataManager.getAllPersonsWithActiveRemote();
 		
 		for (Person person : allPersons) 
-			listeners.add(new User(person, person.getRemote(), this));				
+			users.add(new User(person, person.getRemote(), this));				
 	}
 	
 	/**
 	 * Add- & remove function for observer pattern
 	 **/
 	@Override
-	public void addObserver(AdminObserver o) {
-		this.listeners.add(o);
+	public void addObserver(User o) {
+		this.users.add(o);
 	}
 	@Override
-	public void removeObserver(AdminObserver o) {
-		this.listeners.remove(o);
+	public void removeObserver(User o) {
+		this.users.remove(o);
 	}
 
 	/**
 	 * Notify function for observer pattern
 	 **/
-	@Override
-	public void notifyAllObservers() {
-		for (AdminObserver adminObserver : listeners) {
-			adminObserver.update(getFrequency());
+	private void notifyAllObservers() {
+		for (User user : users) {
+			user.update(getFrequency());
 		}
 	}
 
-	public void checkId(User user){
-		if(user.getRemote().getIsActive() && checkDateExpired(user.getPerson().getEndOfContract()))
-			notifyAllObservers();
+	public long checkIdForFrequency(User unknownUser){
+		User userFromList = findUserInList(unknownUser);
+		if ( isDateInFuture( userFromList.getPerson().getEndOfContract() ) )
+			return getFrequency();
+
+		return 0;
 	}
 	
-	private boolean checkDateExpired(Date date){
-		int result = date.compareTo(Date.valueOf(LocalDate.now()));
-		
-		return result >= 0;
+	private User findUserInList(User unknownUser) {
+		for (User user : users) {
+			if (user.getRemote().getId() == unknownUser.getRemote().getId() 
+					&& user.getPerson().getId() == unknownUser.getPerson().getId())
+				return user;
+		}
+		return null;
 	}
 	
 	/**
@@ -109,7 +114,7 @@ public class Administrator implements AdminSubject{
 		person.getRemote().setIsActive(true);
 		DataManager.updatePerson(person); 
 		
-		listeners.add(new User(person, person.getRemote(), this));
+		users.add(new User(person, person.getRemote(), this));
 		return RegisterPersonResult.succesfull.toString();
 	}
 	
@@ -117,61 +122,33 @@ public class Administrator implements AdminSubject{
 	 * Remove existing person (if registered). 
 	 * @return AddRemovePersonResult - Enum which contains possible outcomes of the situation
 	 */
-	public String deActivatePerson(Person person) {
+	public DeactivatePersonResult deActivatePerson(Person person) {
 		User user = findUserInList(person);
 		
 		if (user == null)
-			return DeactivatePersonResult.NotFound.toString();
-		
-		if (user.getPerson().getRemote() == null) // if (User has a remote object)
-			return DeactivatePersonResult.NoRemote.toString();
+			return DeactivatePersonResult.notFound;
 		
 		user.getPerson().getRemote().setIsActive(false);
 		DataManager.updatePerson(person);
-		listeners.remove(user);
-		return DeactivatePersonResult.Succesfull.toString();
+		users.remove(user);
+		return DeactivatePersonResult.succesfull;
+	}	
+
+
+	/**
+	 * @return Boolean - Is the inputted date is in the future
+	 **/
+	private boolean isDateInFuture(Date date){
+		int result = date.compareTo(Date.valueOf(LocalDate.now()));		
+		return result >= 0;
 	}	
 	
-
-	
-	/** 
-	 * Method to check wether a remote with certain serialNumber can receive the correct frequency
-	 * @return long - frequency of the gate, is zero if the serialNumber is invalid or not authorized
-	 **/
-	public long askFrequency(String serialNumber)
-	{		
-		log.info("Remote asking access to gate has serial number: " + serialNumber);
-		User user = findUserInList(serialNumber);
-		if (user == null) {
-			log.info("Not registered, zero frequency returned!" );
-			return 0;
-		}
-		
-		Person person = user.getPerson();
-		Remote remote = person.getRemote();
-		Date now  = Date.valueOf(LocalDate.now());			
-		if (!remote.getIsActive()) {
-			log.trace("- Remote registered to " + person.toString() + ", but not activated!");
-		} else if (person.getEndOfContract().before(now)) {
-			log.trace("- Remote registered to " + person.toString() + ", but user contract has expired!");
-		} else {
-			log.trace("- Remote registered to " + person.toString() + ", contract valid! Updating remote!");
-			user.update(getFrequency());
-			return getFrequency();
-		}				
-		return 0;
-	}
-	
 	/**
-	 * Finds User in list that has the same reference to person as the inputted parameter
+	 * @return User - Finds User in list that has the same reference to person as the inputted parameter
 	 **/
 	private User findUserInList(Person person)
 	{	
-		for (AdminObserver o : listeners) {
-			User user = (User) o;
-			
-			if (user == null) // if (AdminObserver was not castable to User)
-				continue;
+		for (User user : users) {
 			if (user.getPerson() != person) // if (User object does not reference the same person)
 				continue;
 			
@@ -179,33 +156,6 @@ public class Administrator implements AdminSubject{
 			return user;
 		}
 		
-		return null;
-	}
-
-	
-	/**
-	 * Finds User in list that has a remote with the same serial number registered to it
-	 **/
-	private User findUserInList(String serialNumber)
-	{	
-		for (AdminObserver o : listeners) {
-			User user = (User) o;
-			
-			if (user == null || user.getPerson() == null || user.getPerson().getRemote() == null) 
-				continue;
-			String serial = user.getPerson().getRemote().getSerialNumber();
-			if ( serial == null || !serial.equals(serialNumber)) { // if (User object does not reference the same person)
-				log.trace("- Different serial number for remote registered to " + user.getPerson().toString()
-						+ ". Serial number is " + serial == null ? "" : serial.toString());	
-				continue;
-			}
-			
-			// We found the correct User!
-			log.trace("- Serial number registered to " + user.getPerson().toString() + "!");	 
-			return user;
-		}
-
-		log.trace("- No user found with remote with serial number " + serialNumber.toString() + " linked to it!");	
 		return null;
 	}
 }
