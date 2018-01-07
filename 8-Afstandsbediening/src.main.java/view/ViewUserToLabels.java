1f9a12e88530fa5759c40dd00ce5b99f225a4b3a
JBeyens <jefbeyens@gmail.com>
2018-01-07 14:35:26 +0100

    error copy paste

--- 8-Afstandsbediening/src.main.java/view/ViewUserToLabels.java
+++ 8-Afstandsbediening/src.main.java/view/ViewUserToLabels.java
@@ -3,4 +3,16 @@ package view;
 import model.business.User;
 import values.DefaultSettings;
 
-
+public final class ViewUserToLabels {
+	public static void SetUserToEntranceLabels(View view, User user) 
+	{		
+		view.setEntranceTabLblSerialNumberUser(	user.getRemote().getSerialNumber());
+		view.setEntranceTabLblFrequencyUser( 	Double.toString( user.getRemote().getFrequency()));
+		view.setEntranceTabLblRegisteredUser(	user.getRemote().getIsActive());
+		view.setEntranceTabLblFirstNameUser(	user.getPerson().getFirstname());
+		view.setEntranceTabLblLastNameUser(		user.getPerson().getLastname()); 
+		view.setEntranceTabLblEndOfContractUser(DefaultSettings.DATE_FORMAT.format(user.getPerson().getEndOfContract()));
+		view.setEntranceTabLblAddressUser(		user.getPerson().getAdress().toString());
+		view.setEntranceTabLblFrequencyGate(	Double.toString(user.getGate().getFrequency()));
+	}
+}
