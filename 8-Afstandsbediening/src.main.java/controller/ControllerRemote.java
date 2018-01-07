43320469edaa7457b50eed9e8f5c7b0c1e5c5f17
JBeyens <jefbeyens@gmail.com>
2018-01-07 12:48:51 +0100

    unused import

--- 8-Afstandsbediening/src.main.java/controller/ControllerRemote.java
+++ 8-Afstandsbediening/src.main.java/controller/ControllerRemote.java
@@ -4,8 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
-
 import org.apache.log4j.Logger;
 
 import model.business.Administrator;
