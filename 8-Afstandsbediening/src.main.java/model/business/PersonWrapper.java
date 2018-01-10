df6d7e295aaa9a2682dc9cee110d945089f94e1c
MacCrimmon <ben.vandevorst@hotmail.com>
2018-01-10 09:55:39 +0100

    idea implemented

--- 8-Afstandsbediening/src.main.java/model/business/PersonWrapper.java
+++ 8-Afstandsbediening/src.main.java/model/business/PersonWrapper.java
@@ -4,20 +4,58 @@ import model.business.interfaces.AdminObserver;
 import model.entities.Person;
 
 public class PersonWrapper extends Person implements AdminObserver{
-	private Administrator admin;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Administrator gate;
 	
 	public PersonWrapper(Person person, Administrator admin){
 		//can add ctor in person
 		//super(person);
 		
-		this.admin = admin;
+		//OF
+		
+		setFirstname(person.getFirstname());
+		setLastname(person.getLastname());
+		setAdress(person.getAdress());
+		setRemote(person.getRemote());
+		setEndOfContract(person.getEndOfContract());
+		
+		this.gate = admin;
 	}
 	
+	/**
+	 * Getter for 'Gate/Administrator'
+	 **/
+	public Administrator getGate() {
+		return gate;
+	}
+
+	/**
+	 * Method to let the remote send a signal to the gate to open it
+	 * @return Boolean - Is gate opening or not
+	 **/
+	public boolean openGate(){	
+		return (checkIdForUpdate() && gate.getFrequency() == getRemote().getFrequency());		
+	}
 	
+	private boolean checkIdForUpdate(){
+		//function parameter in admin nog aanpassen indien ok
+		return gate.checkIdForUpdate(this);
+	}
+
+	/**
+	 * Method for observer pattern where the frequency of the user remote can be updated
+	 **/
 	@Override
 	public void update(long frequency) {
-		// TODO Auto-generated method stub
 		getRemote().setFrequency(frequency);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ": remote " + getRemote().getSerialNumber();
 	}
 
 }
